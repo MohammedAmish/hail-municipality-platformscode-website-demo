82143df4e42d431baf25cc8ecf095a6d35184019
MohammedAmish <mohamish.alshammari@gmail.com>
2025-07-08 10:38:25 +0300

    Added Feedback component, modified Rating component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { ButtonModule } from 'primeng/button';
 import { FooterComponent } from './layout/footer/footer.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { RatingComponent } from './layout/rating/rating.component';
+import { FeedbackComponent } from './layout/feedback/feedback.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, FooterComponent, HeaderComponent, RatingComponent],
+  imports: [RouterOutlet, ButtonModule, FooterComponent, HeaderComponent, RatingComponent, FeedbackComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
